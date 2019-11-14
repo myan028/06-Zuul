7f6fe9ca5a5f3fcc4748da5dcd332bd4cb875d7e
myan028 <55149625+myan028@users.noreply.github.com>
2019-11-14 08:56:46 -0800

    Add files via upload
    pre

---
+++ room.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+#include "room.h"
+
+using namespace std;
+
+/*Room:Room(int mop, int sword, int bottle, int headphones, int orb){
+  
+}*/
+
+struct itemStruct{
+  int mop;
+  int sword;
+  int bottle;
+  int headphones;
+  int orb;
+}
+
+//use char pointer for items?? vs vector for each room and inventory?
---
+++ zuulmain.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <string.h>
+#include <iterator>
+#include <vector>
+
+#include //room.h
+
+using namespace std;
+
+
+int main(){
+	
+	struct inventory{//dont use this change
+		int mop = 0;//dont use int you need like hella ifs
+		int sword = 0;
+		int bottle = 0;
+		int headphones = 0;
+		int orb = 0;
+	}
+	
+	cout << "Welcome to Zuul." << endl;
+	while(true){
+		cout << "You are currently in room 1-20." << endl; //cool
+	}
+}
